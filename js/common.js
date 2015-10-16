3c21daac226e6bd59cbca5dacae2361be76680dc
etna-scorpio <etna.scorpio@gmail.com>
2015-10-16 12:32:54 +0300

    send php

--- js/common.js
+++ js/common.js
@@ -18,5 +18,43 @@ head.ready(function() {
 	$('.popup').click(function(e) {
 		e.stopPropagation();
 	});
+
+	// form
+	(function () {
+		var popup = $('.js-popup');
+		// welcome
+		$.validate({
+			form : '#form-welcome',
+			onSuccess: function() {
+				post_data = {
+					'surname': $('#form-welcome input[name=surname]').val(),
+					'name': $('#form-welcome input[name=name]').val(),
+					'email': $('#form-welcome input[name=email]').val(),
+					'phone': $('#form-welcome input[name=phone]').val()
+				};
+				// Ajax post data to server
+				$.post('send.php', post_data, function(response) {
+					if (response.type == 'error') {
+						console.log('error');
+					}
+					else {
+						// reset values in all input fields
+						popup.fadeOut();
+						$('#form-welcome').get(0).reset();
+						window.location.href = 'thanks.html';
+					}
+				}, 'json');
+				// localstorage
+				if (typeof(Storage) !== 'undefined') {
+					// Store
+					localStorage.setItem('validate', 'yes');
+				} else {
+					// Sorry! No Web Storage support..
+				}
+				return false;
+			}
+		});
+	}());
 });
+
 
